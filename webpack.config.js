4645f5edf5bdde691cb14b5af8f64784b60b2ad6
Thom Boyer <thom@boyers.org>
2019-09-14 13:04:22 -0600

    Add webpack-dev-server (auto recompile+reload when code changes)

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
+const path = require('path');
+
 module.exports = {
   mode: 'development',
   entry: './src/static/index.js',
   output: {
-    path: __dirname + '/dist',
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/assets/', // where the dev server presents its static assets in URL paths
   },
+  devServer: {
+    contentBase: './dist', // where the dev server gets its static assets from the filesystem
+  },
+  //  NOTE: "webpack-dev-server" compiles bundle.js into a tmp file, then serves
+  //  it from memory. It doesn't actually write to dist/bundle.js.  If you want
+  //  your HTML to load code from "file://.../dist/bundle.js", instead of from
+  //  dev server's "http://.../assets/bundle.js", then compile with "webpack".
   module: {
     rules: [
       {
